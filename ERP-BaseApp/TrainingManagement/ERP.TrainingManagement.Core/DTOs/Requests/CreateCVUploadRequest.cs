b792b6a0f757019a60da8fc325d832dd2ad164ff
Pasindu2000B <pasindupremasirilk@gmail.com>
2024-07-07 15:53:22 +0530

    Initial commit

---
+++ ERP-BaseApp/TrainingManagement/ERP.TrainingManagement.Core/DTOs/Requests/CreateCVUploadRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.TrainingManagement.Core.DTOs.Requests
+{
+    public class CreateCVUploadRequest
+    {
+        [FromForm(Name = "StudentId")]
+        public string StudentId { get; set; }
+
+        [FromForm(Name = "File")]
+        public IFormFile File { get; set; }
+       
+    }
+}
